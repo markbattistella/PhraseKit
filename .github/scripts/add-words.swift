c0e2ebd951b50680ddcdfb8aac3db34fdb5ebb42
Mark Battistella <mark_battistella+github@hotmail.com>
2024-08-16 18:43:39 +1000

    2024-08-16

--- .github/scripts/add-words.swift
+++ .github/scripts/add-words.swift
@@ -2,10 +2,6 @@ import Foundation
 
 func main() {
 
-    if let body = ProcessInfo.processInfo.environment["GH_BODY"] {
-        print("body:", body)
-    }
-
     if let wordList = ProcessInfo.processInfo.environment["WORDS"] {
         print("word:", wordList)
     }
